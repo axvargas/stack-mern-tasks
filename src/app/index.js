86c08db527156b904407e03d3f633d4d7c630ac1
axvargas <axvargas@fiec.espol.edu.ec>
2020-04-28 22:26:27 -0500

    config de webpack.config y .babelrc app/index

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert('react goes here!');
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+
+
+render(<App/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/app/index.js',                
-    output: {
-        path: __dirname + '/src/public',
+    entry: path.join(__dirname, 'src', 'app', 'index.js'),                    //esta es mi entrada del codigo
+    output: {                                               //la salida se la define como un archivo llamada bundle.js
+        path:path.join(__dirname, 'src', 'public'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                use: 'babel-loader',                // Hago que use el babel loader debido a que uso jsx
+                test: /\.js$/,                      // Cargue todos los archivos que terminen en .js(es una regex expression)
+                exclude: /node_modules/            // Excepto los que estan en la carpeta /node_modules
+            }
+        ]
     }
 };
