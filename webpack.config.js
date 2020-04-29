0d5423090fc7d75722e1257d68ba013072ba477c
axvargas <axvargas@fiec.espol.edu.ec>
2020-04-29 11:42:04 -0500

    integreacion de MAterialUI con Webpack y babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,27 @@ const path = require('path');
 module.exports = {
     entry: path.join(__dirname, 'src', 'app', 'index.js'),                    //esta es mi entrada del codigo
     output: {                                               //la salida se la define como un archivo llamada bundle.js
-        path:path.join(__dirname, 'src', 'public'),
+        path: path.join(__dirname, 'src', 'public'),
         filename: 'bundle.js'
     },
     module: {
-        rules:[
+        rules: [
             {
                 use: 'babel-loader',                // Hago que use el babel loader debido a que uso jsx
                 test: /\.js$/,                      // Cargue todos los archivos que terminen en .js(es una regex expression)
                 exclude: /node_modules/            // Excepto los que estan en la carpeta /node_modules
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
             }
         ]
     }
+
 };
