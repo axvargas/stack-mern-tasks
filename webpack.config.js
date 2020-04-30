be0e2d3dd62e59daed26c8d3e7d8308f6a788e25
axvargas <axvargas@fiec.espol.edu.ec>
2020-04-30 15:59:06 -0500

    final one

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
         path: path.join(__dirname, 'src', 'public'),
         filename: 'bundle.js'
     },
+
+    devServer:{
+        port: 3000,
+    },
     module: {
         rules: [
             {
